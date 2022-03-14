52529391e813a52a0bc8eaf12a73207565f8b70f
Ferdinand <trendelenburger19.04@googlemail.com>
2022-03-14 15:07:43 +0100

    added csv Description

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 python3 -m venv venv
-source venv/bin/activate & pip install wheel & pip install -r requirements.txt & gunicorn --bind 0.0.0.0:5000 wsgi:app
+source venv/bin/activate & pip install wheel & pip install -r requirements.txt
 
 
